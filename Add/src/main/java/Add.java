6de6877b4e806efca97243139d1459077caf21a3
LiyaJohn95 <liyajohn2@gmail.com>
2018-01-19 19:44:47 +0530

    Add files via upload

---
+++ Add/src/main/java/Add.java
@@ -0,0 +1,31 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class Add
+ */
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		res.setContentType("text/html");
+		int i = Integer.parseInt(req.getParameter("a"));
+		int j = Integer.parseInt(req.getParameter("b"));
+		
+		int k = i + j;
+		PrintWriter out = res.getWriter();
+		out.println("the value is : "+k);
+
+}
+
+}
